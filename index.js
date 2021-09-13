149dd272e64fb1ed63ba10379fec7d335351d4a0
Drew Keat <thedrewkeat@gmail.com>
2021-09-13 15:40:48 -0600

    commit tree 54a357b3b025e4ea10abe4164cb5f65f98292095

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+//returns an array of thank you messages for each name provided to the function
+function writeCards(array, event){
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+        arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr
+}
+
+// invokes console.log once for each number, counting down from the number provided to zero
+function countDown(num) {
+    let i = num
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
+
+
+// logs each number when counting down, starting from the number provided
